129673fe27b1014de39ec98983b872820d5ed338
Olger Garita Torres <olgeragt@gmail.com>
2021-04-12 03:18:33 +0000

    Ejercicio completo 21

--- exercises/21-The-Beatles/app.py
+++ exercises/21-The-Beatles/app.py
@@ -1,6 +1,16 @@
 # Your code here!!
+def sing():
+    for i in range(0,4):
+     print("let it be,")
+    for i in range(1):
+     print("whisper words of wisdom, let it be, let it be,")   
+    for i in range(0,3):
+     print("let it be,")
+    for i in range(1):
+     print("there will be an answer, let it be")   
 
 
 
 
 
+sing()
---
+++ exercises/22-Bottles-Of-Milk/app.py
@@ -0,0 +1,25 @@
+# Your code here!!
+def sing():
+    for i in range(0,1):
+     print("99 bottles of milk on the wall, 99 bottles of milk.")
+     print("Take one down and pass it around, 98 bottles of milk on the wall.")
+     print("")
+    for i in range(0,1):
+     print("98 bottles of milk on the wall, 98 bottles of milk.") 
+     print("Take one down and pass it around, 97 bottles of milk on the wall.")  
+    for i in range(0,1):
+     print("...")
+     print("...")
+    for i in range(0,1):
+     print("1 bottle of milk on the wall, 1 bottle of milk.")
+     print("Take one down and pass it around, no more bottles of milk on the wall.")
+     print("")
+    for i in range(0,1):
+     print("No more bottles of milk on the wall, no more bottles of milk.")
+     print("Go to the store and buy some more, 99 bottles of milk on the wall.")      
+
+
+
+
+
+sing()
